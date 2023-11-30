25c83ba1a688b93573a19d06203d0717d6c3eeae
Abu Osama <abuosamasports@gmail.com>
2023-11-30 18:25:05 +0530

    server created and connected to db

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const connection = require("./Config/db");
+require("dotenv").config();
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/",(re,res)=>{
+    res.send("Welcome to home page")
+})
+
+app.listen(process.env.port, async () => {
+  try {
+    await connection;
+    console.log("db is connected");
+  } catch (error) {
+    console.log("db is not connected");
+  }
+  console.log(`http://localhost:${process.env.port}`);
+});
