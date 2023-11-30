b8db2c79559601752bb9394f48c831f0fece2ee1
Abu Osama <abuosamasports@gmail.com>
2023-11-30 21:26:34 +0530

    Get POst Delete Edit Routes for book  added

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const app = express();
 const connection = require("./Config/db");
 require("dotenv").config();
 const cors = require("cors");
+const { bookRouter } = require("./Routes/book.route");
 
 app.use(cors());
 app.use(express.json());
 
-
-
+app.get("/",(re,res)=>{
+    res.send("Welcome to home page,change route to get details")
+})
+app.use(bookRouter);
 app.listen(process.env.port, async () => {
   try {
     await connection;
